9d0cb7a7dad35150d3fc57888348fd6755042969
Wien Wu <wien.wuwei@gmial.com>
2019-06-12 09:30:06 +0800

    feat: bind prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaController } from './prisma/prisma.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule],
+  controllers: [AppController, PrismaController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
